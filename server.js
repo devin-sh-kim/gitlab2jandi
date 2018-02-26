5b3e365706fd88b87e43f75736622eab3406dc8a
Sanghoon Kim <ujacha.factory@gmail.com>
2018-02-27 00:01:23 +0900

    add commits

--- server.js
+++ server.js
@@ -1,59 +1,68 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');        // call express
+
+var app = express();                 // define our app using express
+
 var bodyParser = require('body-parser');
 var request = require('request');
 
+const jandiWebhook = 'https://wh.jandi.com/connect-api/webhook/18119/02f6f0884f014e2a4356f348e065cacc';
 
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 38080;        // set our port
 
-app.use('/webhook', function(req, res) {
-  
+app.use('/webhook', function (req, res) {
+
 //  console.log(req, res);
 
-  var gBody = req.body;
+    var gBody = req.body;
 
-//  console.log(gBody);
+    // console.log(gBody);
 
+    var data = {};
 
-  var json = {
-    body: '[[' + gBody.project.name + ']](' + gBody.project.web_url + ') ' + gBody.user_name + '님이 Push 했습니다.',
-//    connectColor: '#FAC11B',
-//    connectInfo: [
-//      {
-//        title: '',
-//        description: ''
-//      }
-//    ]
-  };
+    var message = '[' + gBody.project.name + '] ' + gBody.user_name + '님이 Push 했습니다.';
 
+    data.body = message;
 
-  var options = {
-    method: 'POST',
-    url: 'https://wh.jandi.com/connect-api/webhook/18119/943b8debc4b7db04ee67d7b51297fcf7',
-    headers: { 
-      'Accept': 'application/vnd.tosslab.jandi-v2+json',
-      'Content-Type': 'application/json'
-    },
-    json: json
-  };
+    var commits = gBody.commits;
 
-
-  request(options, function(error, response, body){
-    if (!error && response.statusCode == 200) {
-      //var info = JSON.parse(body);
-      //console.log(info.stargazers_count + " Stars");
-      //console.log(info.forks_count + " Forks");
+    if(commits){
+        data.connectColor = '#FAC11B';
+        var connectInfos = [];
+        for(var i = 0; i < commits.length; i++){
+            var commit = commits[i];
+            var connectInfo = {};
+            connectInfo.title = '[' + commit.id + '](' + commit.url + ')';
+            connectInfo.description = commit.message;
+            connectInfos.push(connectInfo);
+        }
+        data.connectInfo = connectInfos;
     }
-  });
+
+    var options = {
+        method: 'POST',
+        url: jandiWebhook,
+        headers: {
+            'Accept': 'application/vnd.tosslab.jandi-v2+json',
+            'Content-Type': 'application/json'
+        },
+        json: data
+    };
 
 
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            //var info = JSON.parse(body);
+            //console.log(info.stargazers_count + " Stars");
+            //console.log(info.forks_count + " Forks");
+        }
+    });
 
-  
-  res.status(200).end();
+
+    res.status(200).end();
 
 
 });
